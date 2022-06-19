b47173235acb01c7f203a37969a975b5e49edc37
Sridhar Y <84130723+ysridhar2000@users.noreply.github.com>
2022-06-19 15:10:39 +0530

    Added controllers.

---
+++ dotnetapp/WebApp/Models/Profiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApp.Models
+{
+    public class Profiles
+    {
+        public string username { get; set; }
+        public string address { get; set; }
+        public string mobile { get; set; }
+        public int loanid { get; set; }
+        public string email { get; set; }
+        public int emi { get; set; }
+    }
+}
